1689dcc9547f4bce855e6c20a7b0345b38ec2d8a
Tang Rufus <tangrufus@gmail.com>
2019-05-25 19:55:43 +0100

    :crown::crown::crown: Keep calm and carry on

---
+++ src/Plugin.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Itineris\RootsWordPressAnnouncement;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\ScriptEvents;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    /** @var IOInterface */
+    protected $io;
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->io = $io;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            ScriptEvents::POST_INSTALL_CMD => array(
+                'announce',
+            ),
+            ScriptEvents::POST_UPDATE_CMD => array(
+                'announce',
+            ),
+        );
+    }
+
+    public function announce()
+    {
+        if (PHP_VERSION_ID >= 50620) {
+            return;
+        }
+
+        $message = 'You PHP version is not compatible with WordPress v5.2 or later.';
+        $message .= ' Composer version constraints failed to catch this because of a mistake in roots/wordpress.';
+        $message .= ' See: https://github.com/roots/wordpress/pull/9';
+
+        $this->io->writeError("<error>${message}</error>");
+    }
+}
